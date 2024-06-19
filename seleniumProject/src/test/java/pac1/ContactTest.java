92f90c3af775c2d707d43b863bd6a6747fc9d12c
Subhamv7 <svdjmix7@gmail.com>
2024-06-19 12:18:41 +0530

    Adding to New_Framework_Repo

---
+++ seleniumProject/src/test/java/pac1/ContactTest.java
@@ -0,0 +1,29 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+
+	@Test
+	public void createContactTest() {
+
+		String BROWSER = System.getProperty("browser");
+		String URL = System.getProperty("url");
+		String USERNAME = System.getProperty("username");
+		String PASSWORD = System.getProperty("password");
+		
+		
+		System.out.println(BROWSER);
+		System.out.println(URL);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+
+		System.out.println("execute Contact test");
+	}
+
+	@Test
+	public void modifyContactTest() {
+
+		System.out.println("execute modify Contact test");
+	}
+}
---
+++ seleniumProject/src/test/java/pac2/OrgTest.java
@@ -0,0 +1,21 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	
+	@Test
+	
+	public void createOrgTest() {
+		
+		
+		System.out.println("executed org test");
+	}
+	
+	@Test
+	public void modifyOrgTest() {
+		
+		System.out.println("modified org test");
+	}
+
+}
